6c51b2e5e0260d685b86e5b4a6730ea8bdf236be
akms-limon <akmslimon@gmail.com>
2023-07-13 00:16:30 +0600

    pc

---
+++ codeforces/B_Rudolph_and_Tic_Tac_Toe.cpp
@@ -0,0 +1,87 @@
+/**
+ *    author: A K M S Limon
+ *    created: 10-July-2023  04:06:48
+**/
+#include <bits/stdc++.h>
+#define nl "\n"
+#define nll cout<<"\n"
+#define ll long long
+#define pb push_back
+#define vc vector
+#define vi vc<int>
+#define vl vc<ll>
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define tc int t; cin>>t; while(t--)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rvs(v) reverse(v.begin(), v.end())
+#define MOD 1000000007
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/gcd(a,b)
+#define PI 3.14159
+#define pii pair<int,int>
+#define dbg(x) cerr<<x<<"\n"
+#define mp(x,y) make_pair(x,y)
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define fl(i,a,b) for(int i = a; i <= b; i++)
+#define fll(i,a,b) for(int i = a; i >= b; i--)
+#define ff first
+#define ss second
+#define flit(v) for(auto it=v.begin(); it!=v.end(); it++)
+#define cinx(array) { for (auto &x: array) cin >> x; }
+#define coutx(array) { for (auto x : array) cout << x << " "; cout << endl; }
+#define sumx(array,sum) { for (auto x: array) sum+=x;  }
+using namespace std;
+
+int isSubstring(string s, string sub) {
+    if (s.find(sub) != string::npos)
+       return s.find(sub);
+    return -1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    tc{
+        string s[3][3];
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<3; j++){
+                char c; cin>>c;
+                s[i][j]=c;
+            }
+        }
+        int x = 0, o = 0, p = 0;
+        for(int i = 0; i<3; i++){
+            int a=0, b=0, c=0;
+            for(int j = 0; j<3; j++){
+                if(s[i][j]=="+") c++;
+                else if(s[i][j]=="O") b++;
+                else if(s[i][j]=="X") a++;
+            }
+            x = max(a, x), o = max(b, o); p = max(p, c);
+        }
+        for(int i = 0; i<3; i++){
+            int a=0, b=0, c=0;
+            for(int j = 0; j<3; j++){
+                if(s[j][i]=="+") c++;
+                else if(s[j][i]=="O") b++;
+                else if(s[j][i]=="X") a++;
+            }
+            x = max(a, x), o = max(b, o); p = max(p, c);
+        }
+        int a=0, b=0, c=0;
+        for(int i = 0, j = 0; i<3; i++, j++){
+            if(s[j][i]=="+") c++;
+            else if(s[j][i]=="O") b++;
+            else if(s[j][i]=="X") a++;
+        }
+        x = max(a, x), o = max(b, o); p = max(p, c);
+        if(x==3) cout<<"X"<<'\n';
+        else if(o==3) cout<<"O"<<'\n';
+        else if(p==3) cout<<'+'<<'\n';
+        else cout<<"DRAW"<<'\n';
+    }
+    return 0;
+}
