fd939ea78146bc3c0e2f4e5c17faa3fac2cda5ea
akms-limon <akmslimon@gmail.com>
2024-01-28 01:43:21 +0600

    phase 1

--- You Know Who Academy/Phase-2/Number theory/J_-_Just_Solve_It_4.cpp
+++ You Know Who Academy/Phase-2/Number theory/J_-_Just_Solve_It_4.cpp
@@ -9,11 +9,22 @@
 #define coutx(array) { for (auto x : array) cout << x << " "; cout << endl; }
 using namespace std;
 
+ll euclid_gcd(ll a, ll b) {
+    if (a == 0) return b;
+    return euclid_gcd(b % a, a);
+}
+
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    tc{
-        
+    ll n, m, l, r;
+    cin >> n >> m >> l >> r;
+    if (n/euclid_gcd(n,m)>r/m){
+        cout<<0<<endl;
+    }
+    else    {
+        ll lc = (n / euclid_gcd(n, m)) * m;
+        cout << (r / lc) - (l -1) / lc << '\n';
     }
     return 0;
 }
